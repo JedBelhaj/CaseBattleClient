6b50b48710b418c17e0f60493d81538cd1e34b2c
JedBelhaj <jedbelhaj05@gmail.com>
2025-05-09 00:21:22 +0100

    testing purposes

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
 const backendURL = import.meta.env.VITE_BACKEND;
+console.log(backendURL);
 
 const socket = io(backendURL);
+console.log(socket);
 
 export default socket;
